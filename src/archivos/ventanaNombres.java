a0fd98da77ee984792b4a4d84e07d91dfae92f87
Endernoob09 <20180309@ricaldone.edu.sv>
2023-06-15 14:16:04 -0600

    Mi nombre jeje

--- src/archivos/ventanaNombres.java
+++ src/archivos/ventanaNombres.java
@@ -29,6 +29,7 @@ public class ventanaNombres extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jToggleButton1 = new javax.swing.JToggleButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,6 +42,10 @@ public class ventanaNombres extends javax.swing.JFrame {
 
         jToggleButton1.setText("Procesar");
         jPanel1.add(jToggleButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 340, 270, 130));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel2.setText("Karla Renee Rivas Hernández   La mas linda ");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 300, 110));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -95,6 +100,7 @@ public class ventanaNombres extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
